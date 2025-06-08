0e71ad400b34c1f08f9af9da885007ed56706448
gwjang <zmdlgl@gmail.com>
2025-06-08 17:37:09 +0900

    init

---
+++ src/main/kotlin/gwjang/trainticket/TrainTicketApplication.kt
@@ -0,0 +1,11 @@
+package gwjang.trainticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainTicketApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainTicketApplication>(*args)
+}
---
+++ src/test/kotlin/gwjang/trainticket/TrainTicketApplicationTests.kt
@@ -0,0 +1,13 @@
+package gwjang.trainticket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainTicketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
